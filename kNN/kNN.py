2aabd01af519dc4cc9aac44ec73300c149e33ebf
abdurrahman4127 <whoamiiammr.nobody@gmail.com>
2023-04-08 01:49:45 +0600

    comment added

--- kNN/kNN.py
+++ kNN/kNN.py
@@ -4,11 +4,12 @@ class kNN:
     def __init__(self, k):
         self.k = k
 
-    # 
+    # skipping 1st line of dataset (as it contains header)
     def read_dataset(self, file_path):
         data = np.genfromtxt(file_path, delimiter=",", skip_header=1)
         self.X = data[: , :4]              # 1st 4 cols (might be float)
         self.y = data[: , 5].astype(int)   # output col = 0 or 1 (integer)
+        
         
     # shuffling the dataset (mapping with index)
     def shuffle_dataset(self):
@@ -16,7 +17,8 @@ class kNN:
         self.X = self.X[all_indices]
         self.y = self.y[all_indices]
         
-
+        
+    # splitting the dataset into 80% - 20% 
     def split_dataset(self, ratio=0.8):
         split_size = int(len(self.X) * (ratio))
         self.X_train = self.X[:split_size]   # first 80% of X
@@ -25,6 +27,7 @@ class kNN:
         self.y_test = self.y[split_size:]    # remaining 20% of y
 
 
+    # the kNN algorithm
     def predict_dataset(self, X_test, k=5):
         y_test_predicted = np.zeros(len(X_test), dtype=int)
         
@@ -32,12 +35,13 @@ class kNN:
             x_test = X_test[i]
             distance = np.sqrt(np.sum((self.X_train - x_test) ** 2, axis=1)) # eucleadian dist
             min_dist_indices = np.argsort(distance)[:k]       # indices of k nearest neighbors
-            y_neighbor = self.y_train[min_dist_indices]       # classes of k nearest neighbors
+            y_neighbor = self.y_train[min_dist_indices]       # output (y) of k nearest neighbors
             y_test_predicted[i] = np.bincount(y_neighbor).argmax()  # most occurance
         
         return y_test_predicted
     
     
+    # measuring the accuracy of the dataset
     def get_result(self):
         y_test_predicted = self.predict_dataset(self.X_test)
         accurate_prediction = 0
